f6ca37873092c46e9e622b443a1981ad18a1e1ab
Immijimmi <immijimmi1@gmail.com>
2020-12-20 12:07:30 +0000

    Updated board layout, removed test environment boilerplate

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ class Config:
     ICON_FILENAME = r"res/icon.ico"
 
     INITIAL_BOARDS_VISIBLE = {BoardController, Tips}
-    BOARDS_LAYOUT = {  # row and column must be provided. rowspan and columnspan are defaulted to 1 if not provided
-        BoardController: {"row": 0, "column": 0, "rowspan": 3},
-        Actuals: {"row": 0, "column": 1, "columnspan": 2},
-        Schedule: {"row": 1, "column": 1, "columnspan": 2},
-        Tips: {"row": 2, "column": 1, "columnspan": 2},
-        File: {"row": 0, "column": 3}
+    BOARDS_LAYOUT = {  # Row and column must be provided. Rowspan and columnspan are defaulted to 1 if not provided
+        BoardController: {"row": 0, "column": 0, "rowspan": 2},
+        Actuals: {"row": 0, "column": 1, "columnspan": 5},
+        Schedule: {"row": 1, "column": 1, "columnspan": 6},
+        Tips: {"row": 2, "column": 1, "columnspan": 5},
+        File: {"row": 0, "column": 6}
     }
--- tracker/components/__init__.py
+++ tracker/components/__init__.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.insert(0, "C:/Repos/tkComponents")
-
 from tkComponents import Component
 from tkComponents.extensions import GridHelper
 from tkComponents.basicComponents import DateStepper, NumberStepper, ToggleButton,\
