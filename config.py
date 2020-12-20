e317b8d73e38372aa9b8761c7aec752d512f0096
Immijimmi <immijimmi1@gmail.com>
2020-12-20 16:17:44 +0000

    Tweaked Boards Config

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Config:
 
     INITIAL_BOARDS_VISIBLE = {BoardController, Tips}
     BOARDS_LAYOUT = {  # Row and column must be provided. Rowspan and columnspan are defaulted to 1 if not provided
-        BoardController: {"row": 0, "column": 0, "rowspan": 2},
-        Actuals: {"row": 0, "column": 1, "columnspan": 5},
-        Schedule: {"row": 1, "column": 1, "columnspan": 6},
-        Tips: {"row": 2, "column": 1, "columnspan": 5},
-        File: {"row": 0, "column": 6}
+        BoardController: {"row": 0, "column": 0, "rowspan": 3},
+        Actuals: {"row": 0, "column": 1, "rowspan": 2, "columnspan": 5},
+        Schedule: {"row": 2, "column": 1, "rowspan": 2, "columnspan": 6},
+        Tips: {"row": 4, "column": 1, "columnspan": 4},
+        File: {"row": 0, "column": 6, "rowspan": 2, "columnspan": 2}
     }
